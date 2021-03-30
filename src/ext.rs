071c8a07b965ac295284a5300e175074d2ef057d
nikos efthias <nikos@mugsoft.io>
2021-03-30 04:12:25 +0300

    parsed query

--- src/ext.rs
+++ src/ext.rs
@@ -1,6 +1,6 @@
 use crate::Query;
 use hyper::Request;
-use std::collections::HashMap;
+use std::{collections::HashMap, str::FromStr};
 
 /// An extension trait which extends the [`hyper::Request`](https://docs.rs/hyper/0.13.5/hyper/struct.Request.html) type with some helpful methods to
 /// access query values from `req` object.
@@ -78,6 +78,45 @@ pub trait RequestQueryExt {
     /// # run();
     /// ```
     fn query<P: Into<String>>(&self, query_name: P) -> Option<&String>;
+
+    /// It returns the query value by a query name parsed into T.
+    /// Returns Some(FromStr::Err) if parsing was unsuccessful.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use hyper::{Body, Request, Response, Server};
+    /// use routerify::{Router, RouterService};
+    /// // Import the query_parser function and the RequestQueryExt trait.
+    /// use routerify_query::{query_parser, RequestQueryExt};
+    /// use std::{convert::Infallible, net::SocketAddr};
+    ///
+    /// // A handler for "/" page. Visit: "/?username=Alice&bookname=HarryPotter&page_read=7&enjoying=true" to see query values.
+    /// async fn home_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    ///     // Access the query values.
+    ///     let user_name = req.query("username").unwrap();
+    ///     let book_name = req.query("bookname").unwrap();
+    ///     let page_read = req.query_parsed::<_,usize>("page_read").unwrap().unwrap_or_default();
+    ///     let enjoying:bool = req.query_parsed("enjoying").unwrap().unwrap_or_default();
+    ///
+    ///     Ok(Response::new(Body::from(format!(
+    ///         "User: {}, Book: {}, Page: {}, Enjoying: {}",
+    ///         user_name, book_name, page_read, enjoying
+    ///     ))))
+    /// }
+    ///
+    /// # fn run() -> Router<Body, Infallible> {
+    /// // Create a router.
+    /// Router::builder()
+    ///   // Attach the query_parser middleware.
+    ///   .middleware(query_parser())
+    ///   .get("/", home_handler)
+    ///   .build()
+    ///   .unwrap()
+    /// }
+    /// # run();
+    /// ```
+    fn query_parsed<P: Into<String>, T: FromStr>(&self, query_name: P) -> Option<Result<T, <T as FromStr>::Err>>;
 }
 
 impl RequestQueryExt for Request<hyper::Body> {
@@ -90,5 +129,9 @@ impl RequestQueryExt for Request<hyper::Body> {
 
     fn query<P: Into<String>>(&self, query_name: P) -> Option<&String> {
         self.queries().get(&query_name.into())
+    }
+
+    fn query_parsed<P: Into<String>, T: FromStr>(&self, query_name: P) -> Option<Result<T, <T as FromStr>::Err>> {
+        self.query(query_name).map(|t| t.parse::<T>())
     }
 }
